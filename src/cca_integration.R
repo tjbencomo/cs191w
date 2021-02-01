187923733a250afc0b5a3f94b2477b4e8c563f53
Tomas Bencomo <tjbencomo@gmail.com>
2021-02-01 02:12:45 -0700

    Updates for CCA integration, more subtyping

--- src/cca_integration.R
+++ src/cca_integration.R
@@ -32,30 +32,73 @@ for (i in 1:length(pni.list)) {
   pni.list[[i]] <- subset(pni.list[[i]], nFeature_RNA > 200 & percent.mt < 20)
   
   pni.list[[i]] <- NormalizeData(pni.list[[i]])
-  pni.list[[i]] <- FindVariableFeatures(pni.list[[i]], selection.method = "vst", nfeatures = 2000)
+  # pni.list[[i]] <- FindVariableFeatures(pni.list[[i]], selection.method = "vst", nfeatures = 2000)
+  pni.list[[i]] <- FindVariableFeatures(pni.list[[i]], selection.method = "vst", nfeatures = 4000)
 }
 
-pni.anchors <- FindIntegrationAnchors(object.list = pni.list, dims = 1:30)
-pni.integrated <- IntegrateData(anchorset = pni.anchors, dims = 1:30)
+# pni.anchors <- FindIntegrationAnchors(object.list = pni.list, dims = 1:30)
+# pni.integrated <- IntegrateData(anchorset = pni.anchors, dims = 1:30)
+pni.anchors <- FindIntegrationAnchors(object.list = pni.list, anchor.features = 4000, dims = 1:50)
+pni.integrated <- IntegrateData(anchorset = pni.anchors, dims = 1:50)
+
 
 DefaultAssay(pni.integrated) <- "integrated"
 
 pni.integrated <- ScaleData(pni.integrated)
-pni.integrated <- RunPCA(pni.integrated, npcs = 30)
-pni.integrated <- FindNeighbors(pni.integrated, dims = 1:30)
-pni.integrated <- FindClusters(pni.integrated, resolution = 0.1)
-pni.integrated <- RunUMAP(pni.integrated, reduction = "pca", dims = 1:30)
+pni.integrated <- RunPCA(pni.integrated, npcs = 50)
+pni.integrated <- FindNeighbors(pni.integrated, dims = 1:50)
+pni.integrated <- FindClusters(pni.integrated, resolution = 0.07)
+pni.integrated <- RunUMAP(pni.integrated, reduction = "pca", dims = 1:50)
+# pni.integrated <- ScaleData(pni.integrated)
+# pni.integrated <- RunPCA(pni.integrated, npcs = 30)
+# pni.integrated <- FindNeighbors(pni.integrated, dims = 1:30)
+# pni.integrated <- FindClusters(pni.integrated, resolution = 0.1)
+# pni.integrated <- RunUMAP(pni.integrated, reduction = "pca", dims = 1:30)
 p1 <- DimPlot(pni.integrated, reduction = "umap", label = T)
+print(p1)
 p2 <- DimPlot(pni.integrated, reduction = "umap", split.by = "orig.ident")
-p1 + p2
+print(p2)
+# p1 + p2
 
 ggsave("figures/cca_umap.eps", p1)
 ggsave("figures/cca_samples.eps", p2, width = 12, height = 6)
 
 pni.markers <- FindAllMarkers(pni.integrated, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
-top_markers <- pni.markers %>% group_by(cluster) %>% top_n(n = 10, wt = avg_logFC)
+top_markers <- pni.markers %>% 
+  filter(p_val_adj < .05) %>%
+  group_by(cluster) %>% 
+  slice_max(avg_logFC, n=15) %>%
+  ungroup()
 
-write_csv(top_markers, "data/markers/cca_markers.csv")
+cluster_labels <- c(
+  "Epithelial cells",
+  "Lymphoid cells",
+  "Endothelial cells",
+  "Fibroblasts",
+  "Myeloid cells",
+  "Epithelial cells",
+  "Lymphoid cells",
+  "Lymphoid cells",
+  "Melanocytes",
+  "Lymphoid cells",
+  "Granulocytes"
+)
+
+names(cluster_labels) <- levels(pni.integrated)
+pni.integrated <- RenameIdents(pni.integrated, cluster_labels)
+
+clusterid2label <- tibble(
+  cluster = factor(0:(length(unique(pni.integrated$seurat_clusters))-1)),
+  label = cluster_labels
+)
+
+pni.markers <- pni.markers %>%
+  left_join(clusterid2label)
+top_markers <- top_markers %>%
+  left_join(clusterid2label)
+
+write_csv(top_markers, "data/markers/top_cca_markers.csv")
+write_csv(pni.markers, "data/markers/all_cca_markers.csv")
 
 ## Save Seurat object
 saveRDS(pni.integrated, file = "data/seurat/cca_pni.rds")
--- src/epithelial_subgroups.R
+++ src/epithelial_subgroups.R
@@ -1,4 +1,14 @@
+# File: epithelial_subgroups.R
+# Description: Isolate epithelial cells and identify
+# subpopulations. Based on Andrew Ji's paper, we are
+# looking for 3 main cell types: 1) pilosebaceous (hair/sebum)
+# cells, 2) eccrine (sweat glands) 3) keratinocytes. After
+# identification the goal is to remove all non-keratinocytes
+# so we can perform KC specific analyses.
+
 library(Seurat)
+library(dplyr)
+library(ggplot2)
 library(readr)
 
 data_dir <- "data"
@@ -15,8 +25,28 @@ epi <- RunUMAP(epi, reduction = "harmony", dims = 1:30)
 
 p1 <- DimPlot(epi, reduction = "umap", label=T)
 print(p1)
+p2 <- DimPlot(epi, reduction = "umap", label = T, split.by = "orig.ident")
+print(p2)
 
 epi.markers <- FindAllMarkers(epi, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
+epi.markers <- epi.markers %>%
+  filter(p_val_adj < .05)
 top_markers <- epi.markers %>% group_by(cluster) %>% top_n(n = 15, wt = avg_logFC)
 
+pilosebaceous_featplot <- FeaturePlot(epi, c("SOX9", "LHX2", "KRT15"))
+print(pilosebaceous_featplot)
+
+eccrine_featplot <- FeaturePlot(epi, c("KRT7", "KRT8", "KRT14", "KRT18", "KRT19", "MUC1", 
+                                       "CEACAM5", "CEACAM6", "CEACAM1"))
+print(eccrine_featplot)
+
+epi <- AddMetaData(epi, epi$seurat_clusters, col.name = "epi_clusters")
+kcs <- subset(epi, epi_clusters != 0 & epi_clusters != 8 & epi_clusters != 9 & epi_clusters != 12)
+
+## We aren't interested in non-KC cells anymore - only save KCs
+# Save Seurat object
+saveRDS(kcs, file = "data/seurat/keratinocytes.rds")
+
+
+ggsave("figures/epi_clusters.eps", p1)
 write_csv(epi.markers, "data/markers/epithelial_subgroup_markers.csv")
