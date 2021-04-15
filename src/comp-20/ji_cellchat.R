2780a64b705e3fd68018f56aff5c11933de908e1
Tomas Bencomo <tjbencomo@gmail.com>
2021-04-15 14:18:22 -0700

    Add Cellchat signaling analysis for more inclusive cell criteria

---
+++ src/comp-20/ji_cellchat.R
@@ -0,0 +1,65 @@
+# File: ji_cellchat.R
+# Description: Perform crosstalk analysis on Ji's single cell data
+
+library(Seurat)
+library(CellChat)
+
+data_dir <- file.path("data", "seurat", "harmony-20")
+cells_fp <- file.path(data_dir, "harmony_combined.rds")
+metadata_fp <- file.path(data_dir, "full_metadata.csv")
+outfp <- file.path(data_dir, "cellchat_ji.rds")
+
+cells <- readRDS(cells_fp)
+metadata <- read_csv(metadata_fp)
+
+
+## Collapse fibro/endo subcategories
+cells2remove <- c("Pilosebaceous/Eccrine cell", "Melanocyte")
+metadata <- metadata %>%
+  filter(keep == "Yes", condition == "cSCC") %>%
+  filter(!(Subpopulation_Label %in% cells2remove)) %>%
+  mutate(Final_Label = case_when(
+    Cell_Type == "Endothelial cells" ~ "Endo",
+    Cell_Type == "Fibroblasts" ~ "Fibro",
+    Cell_Type == "Mast cells" ~ "Mast",
+    Cell_Type == "B & Plasma cells" ~ "B & Plasma",
+    TRUE ~ Subpopulation_Label
+  ))
+
+cells$keep <- rownames(cells@meta.data) %in% metadata$cell_id
+ji <- subset(cells, keep == 1)
+rm(cells)
+ji$cell_label <- metadata$Final_Label
+
+cellchat <- createCellChat(object = ji, group.by = "cell_label")
+cellChatDB <- CellChatDB.human
+cellchat@DB <- cellChatDB
+rm(ji)
+
+if (Sys.getenv("RSTUDIO") != "1") {
+  print("Using multiple cores")
+  future::plan("multicore", workers = 3)
+}
+
+
+cellchat <- subsetData(cellchat)
+cellchat <- identifyOverExpressedGenes(cellchat)
+cellchat <- identifyOverExpressedInteractions(cellchat)
+cellchat <- projectData(cellchat, PPI.human)
+
+print("Computing communication probabilities")
+cellchat <- computeCommunProb(cellchat)
+cellchat <- filterCommunication(cellchat, min.cells = 10)
+
+df.net <- subsetCommunication(cellchat)
+df.net.pathways <- subsetCommunication(cellchat, slot.name = "netP")
+
+cellchat <- computeCommunProbPathway(cellchat)
+
+cellchat <- aggregateNet(cellchat)
+
+## Network Analysis
+cellchat <- netAnalysis_computeCentrality(cellchat, slot.name = "netP")
+
+print("Saving results object")
+saveRDS(cellchat, outfp)
